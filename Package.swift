3730507025fb0fae175a652b82278005ca0b4e35
Arams <antonio.ramos.p@gmail.com>
2024-11-23 05:31:43 +0000

    Reverting

--- Package.swift
+++ Package.swift
@@ -22,11 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DesignSystem",
-            swiftSettings: [
-                // Define for debugging
-                .define("BUILD_FOR_PREVIEWS", .when(configuration: .debug))
-            ]
+            name: "DesignSystem"
         ),
         .testTarget(
             name: "DesignSystemTests",
